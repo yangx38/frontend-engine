2d8fd852c60ee7491f94bb240bb6184056dc4dd7
Xiaotong Yang <syangx38@gmail.com>
2021-05-04 09:50:30 -0700

    May 4: Pay an Invoice

--- src/common/form/index.js
+++ src/common/form/index.js
@@ -3,7 +3,7 @@ import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { actionCreators } from './store';
 import Immutable from 'immutable';
-import { Form, Input, Button, DatePicker, Select, InputNumber, Space, Radio, Upload, Typography } from 'antd';
+import { Form, Input, Button, DatePicker, Select, InputNumber, Space, Radio, Upload, Typography, Divider } from 'antd';
 import { MinusCircleOutlined, PlusOutlined, UploadOutlined } from '@ant-design/icons';
 
 
@@ -43,8 +43,8 @@ class FormForSubmitter extends Component {
         
         return (
             <Form {...layout} name="travelform" initialValues={{ remember: true, }} onFinish={onFinishTravelRequestForm}>
-                <Form.Item label="Legal First Name" name="legalFirstName" rules={[ { required: true, message: 'Please input your legal first name!', }, ]} ><Input /></Form.Item>
-                <Form.Item label="Legal Last Name" name="legalLastName" rules={[ { required: true, message: 'Please input your legal last name!', }, ]} ><Input /></Form.Item>
+                <Form.Item label="Legal First Name" name="legalfirstname" rules={[ { required: true, message: 'Please input your legal first name!', }, ]} ><Input /></Form.Item>
+                <Form.Item label="Legal Last Name" name="legallastname" rules={[ { required: true, message: 'Please input your legal last name!', }, ]} ><Input /></Form.Item>
                 <Form.Item label="Departure" name="departure" rules={[ { required: true, message: 'Please input your departure!', }, ]} ><Input placeholder="City of airport"/></Form.Item>
                 <Form.Item label="Destination" name="destination" rules={[ { required: true, message: 'Please input your destination!', }, ]} ><Input /></Form.Item>
                 <Form.Item label="Departing & Returning Date" name="departingreturningdate" rules={[ { type: 'array', required: true, message: 'Please select time!', }, ]}><RangePicker format="YYYY-MM-DD"/></Form.Item>
@@ -111,24 +111,19 @@ class FormForSubmitter extends Component {
         const { traRei_changeReimbursedBefore, traRei_changeRequestForSelf, traRei_changeWhetherCitizen, traRei_changeWhetherPersonalTravelInclude, normFile, onFinishTravelReimbursementForm } = this.props;
 
         return (
-            <Form {...layout} name="travelform" initialValues={{ remember: true, }} onFinish={onFinishTravelReimbursementForm}>
+            <Form {...layout} name="travelreimbursementform" initialValues={{ remember: true, }} onFinish={onFinishTravelReimbursementForm}>
+                <Divider className='divider'>Travel Reimbursement</Divider>
                 <Form.Item label="Have you been reimbursed before this trip?" name="reimbursedbefore" rules={[ { required: true, message: 'Please input your choice!', }, ]} >
-                    <Radio.Group onChange={traRei_changeReimbursedBefore}>
-                        <Radio value={'yes'}>Yes</Radio>
-                        <Radio value={'no'}>No</Radio>
-                    </Radio.Group>
+                    <Radio.Group onChange={traRei_changeReimbursedBefore}><Radio value={'yes'}>Yes</Radio><Radio value={'no'}>No</Radio></Radio.Group>
                 </Form.Item>
                 { reimbursedBefore === 'yes' ? <Form.Item label="Reference Number" name="referencenumber" ><Input placeholder="Leave Blank If Not Known"/></Form.Item> : null }
                 <Form.Item label="Requesting this reimbursement for yourself?" name="requestforself" rules={[ { required: true, message: 'Please input your choice!', }, ]} >
-                    <Radio.Group onChange={traRei_changeRequestForSelf}>
-                        <Radio value={'yes'}>Yes</Radio>
-                        <Radio value={'no'}>No</Radio>
-                    </Radio.Group>
+                    <Radio.Group onChange={traRei_changeRequestForSelf}><Radio value={'yes'}>Yes</Radio><Radio value={'no'}>No</Radio></Radio.Group>
                 </Form.Item>
                 { 
                     requestForSelf === 'no' ? <Fragment><Form.Item label="Name" name="requestforself_name" rules={[ { required: true, message: 'Please input name!', }, ]} ><Input /></Form.Item>
                         <Form.Item label="Affliation" name="requestforself_affliation" rules={[ { required: true, message: 'Please input afflication!', }, ]} ><Input /></Form.Item>
-                        <Form.Item label="Email" name="requestforself_email" rules={[ { required: true, message: 'Please input email!', }, ]} ><Input /></Form.Item></Fragment> : null 
+                        <Form.Item label="Email" name="requestforself_email" rules={[ { type: 'email', message: 'Not valid email!', }, { required: true, message: 'Please input email!', }, ]} ><Input /></Form.Item></Fragment> : null 
                 }
                 <Form.Item label="Date Submitted" name="datesubmitted" rules={[ { required: true, message: 'Please input date!', }, ]} >
                     <DatePicker />
@@ -188,22 +183,14 @@ class FormForSubmitter extends Component {
                     <div className='uwPolicy'><Typography.Link href="https://finance.uw.edu/travel/responsibility#personal">UW Policy</Typography.Link></div></span>
                     <Space className='firstBudgetRow'>
                         <Form.Item name="whetherpersontravelinclude" rules={[ { required: true, message: 'Select!', }, ]} >
-                        
-                        <Radio.Group onChange={traRei_changeWhetherPersonalTravelInclude}><Space direction="vertical"><Radio value={'yes'}>Yes</Radio><Radio value={'no'}>No</Radio></Space></Radio.Group>
+                            <Radio.Group onChange={traRei_changeWhetherPersonalTravelInclude}><Space direction="vertical"><Radio value={'yes'}>Yes</Radio><Radio value={'no'}>No</Radio></Space></Radio.Group>
                         </Form.Item>
                     </Space>
                 </div>
                 { whetherPersonalTravelInclude === 'yes' ? <Form.Item label="Departing & Returning Time" name="departingreturningtime"><RangePicker showTime format="YYYY-MM-DD HH:mm" /></Form.Item>: null }
 
+                <Divider className='divider'>Travel Costs</Divider>
 
-
-
-
-                {/* <Form.Item label="Registration Receipt" name="registrationreceipt" valuePropName="fileList" getValueFromEvent={normFile}>
-                    <Upload name="file" action="http://localhost:8080/upload" listType="picture">
-                        <Button icon={<UploadOutlined />}>Click to upload</Button>
-                    </Upload>
-                </Form.Item> */}
 
                 <Form.Item {...tailLayout}>
                     <Button type="primary" htmlType="submit">Finish</Button>
@@ -219,9 +206,27 @@ class FormForSubmitter extends Component {
         const { onFinishPayAnInvoiceForm } = this.props;
 
         return (
-            <Form {...layout} name="travelform" initialValues={{ remember: true, }} onFinish={onFinishPayAnInvoiceForm}>
+            <Form {...layout} name="payaninvoiceform" initialValues={{ remember: true, }} onFinish={onFinishPayAnInvoiceForm}>
+                <Divider className='divider'>Shipping Address</Divider>
+                <Form.Item label="Full Name" name="fullname" rules={[ { required: true, message: 'Please input your name!', }, ]} ><Input /></Form.Item>
+                <Form.Item label="Address Line 1" name="addressline1" rules={[ { required: true, message: 'Please input your address!', }, ]} ><Input /></Form.Item>
+                <Form.Item label="Address Line 2" name="addressline2" ><Input /></Form.Item>
+                <Form.Item label="City" name="city" rules={[ { required: true, message: 'Please input city!', }, ]} ><Input /></Form.Item>
+                <Form.Item label="State" name="state" rules={[ { required: true, message: 'Please input state!', }, ]} ><Input /></Form.Item>
+                <Form.Item label="Zip Code" name="zipcode" rules={[ { required: true, message: 'Please input zip code!', }, ]} ><Input /></Form.Item>
+                <Form.Item label="Country" name="country" rules={[ { required: true, message: 'Please input country!', }, ]} ><Input /></Form.Item>
+
+                <Divider className='divider'>Vendor Information</Divider>
+                <Form.Item label="Vendor Name" name="vendorname" rules={[ { required: true, message: 'Please input vendor name!', }, ]} ><Input /></Form.Item>
+                <Form.Item label="Vendor Email" name="vendoremail" ><Input /></Form.Item>
+                <Form.Item label="Vendor Phone" name="vendorphone"><Input /></Form.Item>
+                <Form.Item label="Vendor Website" name="vendorwebsite" ><Input /></Form.Item>
+
+                <Divider className='divider'>Items</Divider>
+                
+
                 <Form.Item {...tailLayout}>
-                    {/* <Button type="primary" htmlType="submit">Finish</Button> */}
+                    <Button type="primary" htmlType="submit">Finish</Button>
                 </Form.Item>
             </Form>
         );
