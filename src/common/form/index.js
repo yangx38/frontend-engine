8948c97fea76176476060dbf644789f505aecb1b
Xiaotong Yang <syangx38@gmail.com>
2021-05-04 02:12:26 -0700

    May 4: Budget List

--- src/common/form/index.js
+++ src/common/form/index.js
@@ -18,27 +18,27 @@ import {
 
 class FormForSubmitter extends Component {
     getTravelRequestForm() {
-        // const { Option } = Select;
-        // const tempData = [
-        //     {
-        //         "budgetnumber": "62-0372",
-        //         "budgetname": "TSUNAMI EVACUATION S"
-        //     },
-        //     {
-        //         "budgetnumber": "66-2729",
-        //         "budgetname": "MIC KCMETRO COST SHARE"
-        //     },
-        //     {
-        //         "budgetnumber": "63-8050",
-        //         "budgetname": "PSRC CASESTDY4SUB CHEN"
-        //     }
-        // ]
-        // var tempDataJS = [];
-        // tempData.map(item => {
-        //     const { budgetnumber, budgetname } = item;
-        //     tempDataJS.push(<Option value={budgetnumber}>{budgetnumber.concat(' - ').concat(budgetname)}</Option>);
-        // })
-        // console.log(tempDataJS)
+        const { Option } = Select;
+        const tempData = [
+            {
+                "budgetnumber": "62-0372",
+                "budgetname": "TSUNAMI EVACUATION S"
+            },
+            {
+                "budgetnumber": "66-2729",
+                "budgetname": "MIC KCMETRO COST SHARE"
+            },
+            {
+                "budgetnumber": "63-8050",
+                "budgetname": "PSRC CASESTDY4SUB CHEN"
+            }
+        ]
+        var tempDataJS = [];
+        tempData.map(item => {
+            const { budgetnumber, budgetname } = item;
+            tempDataJS.push(<Option key={budgetnumber} value={budgetnumber}>{budgetnumber.concat(' - ').concat(budgetname)}</Option>);
+        })
+        console.log(tempDataJS)
         // const templayout = { labelCol: { span: 8, }, wrapperCol: { span: 10, span: 8 }, };
         const onFinish = values => {
             console.log('Received values of form:', values);
@@ -53,7 +53,7 @@ class FormForSubmitter extends Component {
         const { updateFirstName, updateLastName, updateDeparture, updateDestination, updateDepartingAndReturningTime, updateReason } = this.props;
         return (
             <Form {...layout} name="travelform" initialValues={{ remember: true, }} onFinish={onFinish}>
-                {/* <Form.Item label="Legal First Name" name="legalFirstName" rules={[ { required: true, message: 'Please input your legal first name!', }, ]} >
+                <Form.Item label="Legal First Name" name="legalFirstName" rules={[ { required: true, message: 'Please input your legal first name!', }, ]} >
                     <Input onChange={updateFirstName} />
                 </Form.Item>
                 <Form.Item label="Legal Last Name" name="legalLastName" rules={[ { required: true, message: 'Please input your legal last name!', }, ]} >
@@ -70,7 +70,7 @@ class FormForSubmitter extends Component {
                 </Form.Item>
                 <Form.Item label="Reason" name="reason" rules={[ { required: true, message: 'Please input your reason!', }, ]} >
                     <TextArea rows={4} onChange={updateReason} />
-                </Form.Item> */}
+                </Form.Item>
                  {/* <Form.Item label="Budget Number">
                    <Input.Group compact>
                         <Form.Item name={['budget', 'budgetnumber']} noStyle rules={[{ required: true, message: 'Budget is required' }]} >
@@ -79,7 +79,7 @@ class FormForSubmitter extends Component {
                             </Select>
                         </Form.Item>
                         <Form.Item name={['budget', 'budgetamount']} noStyle rules={[{ required: true, message: 'Amount is required' }]} >
-                            <InputNumber style={{ width: '35%' }} formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')} parser={value => value.replace(/\$\s?|(,*)/g, '')} />
+                            
                         </Form.Item>
                         <Form.Item style={{ width: '10%', paddingLeft: '3%' }}>
                         
@@ -88,15 +88,15 @@ class FormForSubmitter extends Component {
                 </Form.Item>*/}
                 <Form.List name="users">
                     {(fields, { add, remove }) => (
-                        <>
+                        <Fragment>
                             {
                                 fields.map(({ key, name, fieldKey, ...restField }) => (
-                                    <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
-                                        <Form.Item {...restField} name={[name, 'first']} fieldKey={[fieldKey, 'first']} rules={[{ required: true, message: 'Missing first name' }]} >
-                                            <Input placeholder="First Name" />
+                                    <Space key={key} align="baseline" >
+                                        <Form.Item className='budgetRow' label="Budget Number" {...restField} name={[name, 'first']} fieldKey={[fieldKey, 'first']} rules={[{ required: true, message: 'Missing first name' }]} >
+                                            <Select className='budgetSelect' placeholder="Select Budget">{tempDataJS}</Select>
                                         </Form.Item>
-                                        <Form.Item {...restField} name={[name, 'last']} fieldKey={[fieldKey, 'last']} rules={[{ required: true, message: 'Missing last name' }]} >
-                                            <Input placeholder="Last Name" />
+                                        <Form.Item {...restField} name={[name, 'amount']} fieldKey={[fieldKey, 'amount']} rules={[{ required: true, message: 'Missing Amount' }]} >
+                                            <InputNumber className='budgetAmount' formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')} parser={value => value.replace(/\$\s?|(,*)/g, '')} />
                                         </Form.Item>
                                         <MinusCircleOutlined onClick={() => remove(name)} />
                                     </Space>
@@ -105,7 +105,7 @@ class FormForSubmitter extends Component {
                             <Form.Item>
                                 <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}> Add field</Button>
                             </Form.Item>
-                        </>
+                        </Fragment>
                     )}
                 </Form.List>
                 <Form.Item {...tailLayout}>
